690a1821834ab3050c8c867f93bd307aebff028e
Haofan Wang <frankmiracle@outlook.com>
2021-09-21 00:03:14 +0800

    Update __init__.py

--- cropimage/__init__.py
+++ cropimage/__init__.py
@@ -4,4 +4,4 @@ from .cropper import Cropper
 __title__ = "cropimage"
 __description__ = "Automatically crops main body from pictures"
 __author__ = "Haofan Wang"
-__version__ = "0.0.1"
+__version__ = "0.0.3"
